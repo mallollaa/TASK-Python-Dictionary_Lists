9584be6670d6f65131a191943fda478fda6d6d2b
mallollaa <mah41411@gmail.com>
2023-01-10 20:04:18 +0300

    not done yet

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ print(books[0])
 #  recieves a book dictionary
 #  returns the number of authors that the book has
 def number_of_authors(book):
-    ...
+    return len(books["authors"])
 
 
 print(number_of_authors(books[0]))
@@ -15,7 +15,9 @@ print(number_of_authors(books[0]))
 #  # recieves a list of book dictionaries
 #  # returns the book dictionary with the same id as the book_id provided
 def get_book_by_id(book_id, books):
-    ...
+    for book in books :
+        if book_id == book["id"] :
+            return book
 
 
 print(get_book_by_id(38, books))
@@ -27,9 +29,10 @@ print(get_book_by_id(38, books))
 # adds the summary to the book dictionary
 # return the book dictionary
 def add_summary_to_book(summary, book):
-    ...
+    books["summary"] = summary # adding key to the dictnary 
+    return book 
 
-
+print(books[0])
 print(add_summary_to_book("this is a good book about", books[0]))
 
 
